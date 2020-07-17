51950743d71d24e19143aca21859b091b851fdf8
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-17 14:56:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -52,8 +52,8 @@ def add_characters
    }
 
 epic_tragedy[:additional_characters] = []
-[:additional_characters] = {:name => "Prince Escalus"}, {:name => "Apothecary"}
-
+epic_tragedy[:additional_characters] << {:name => "Prince Escalus"}
+epic_tragedy[:additional_characters] << {:name => "Apothecary"}
 
 
 
